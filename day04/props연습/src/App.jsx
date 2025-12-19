24194d18f3fc08c7c409500090733ba1a9570521
kyungmin <kmin.life2@gmail.com>
2025-12-19 15:12:17 +0900

    props의 이름을 key로 저장

--- day04/props연습/src/App.jsx
+++ day04/props연습/src/App.jsx
@@ -1,18 +1,36 @@
-import { Component, useState } from "readt";
+import { Component, useState } from "react";
 import "./App.css";
 
-function functionApp() {
+function FunctionApp() {
   const [counter, setCounter] = useState(0);
   return (
     <>
-      <Count counter={counter} />
+      <Count
+        counter={counter}
+        hello={"hellow"}
+        array={[1, 2, 3, "안녕하세요"]}
+      />
       <button
         onClick={() => {
           setCounter((prev) => prev + 3);
         }}
       >
+        {" "}
         +
+      </button>
+      <button
+        onClick={() => {
+          setCounter((prev) => prev - 3);
+        }}
+      >
+        {" "}
+        -
       </button>
     </>
   );
 }
+function Count(props) {
+  console.log("props", props);
+  return <div>counter : {props.counter}</div>;
+}
+export default FunctionApp;
