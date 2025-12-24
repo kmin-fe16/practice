c20c5591f08210743d01c220756a04b6f1fd4caf
kyungmin <kmin.life2@gmail.com>
2025-12-24 15:25:31 +0900

    feat: update routes2 navigation

--- routes2/src/App.jsx
+++ routes2/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import {
+  Link,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
+  console.log(location.pathname);
+  console.log(location.search);
+
   return (
     <>
       <div>
-        <Link to="/main">메인 </Link>
+        {/* <Link to="/main">메인 </Link>
         <Link to="/mypage">마이페이지 </Link>
-        <Link to="/test">테스트 </Link>
+        <Link to="/test">테스트 </Link> */}
+        <button onClick={() => navigate("/main")}>메인</button>
+        <button onClick={() => navigate("/mypage")}>마이페이지</button>
+        <button onClick={() => navigate("/test")}>테스트</button>
+      </div>
+      <div>
+        <button on onClick={() => navigate(-1)}>{`<-뒤로가기`}</button>
+        <button on onClick={() => navigate(+1)}>{`앞으로가기->`}</button>
       </div>
 
       <Routes>
